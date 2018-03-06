2b876b99461116b590b9c40872260ab01092e079
Guy Sherman <guysherman@Guys-MBP-3.home>
2018-03-06 13:07:37 +0000

    Adding rebuilt pango libs

--- pango/prebuilt/android/armeabi/glib-2.0/include/glibconfig.h
+++ pango/prebuilt/android/armeabi/glib-2.0/include/glibconfig.h
@@ -10,7 +10,6 @@
 
 #include <limits.h>
 #include <float.h>
-#define GLIB_HAVE_ALLOCA_H
 
 G_BEGIN_DECLS
 
@@ -75,6 +74,8 @@ typedef gint64 goffset;
 #define G_GOFFSET_FORMAT        G_GINT64_FORMAT
 #define G_GOFFSET_CONSTANT(val) G_GINT64_CONSTANT(val)
 
+#define G_POLLFD_FORMAT "%d"
+
 
 #define GPOINTER_TO_INT(p)	((gint)  (gint) (p))
 #define GPOINTER_TO_UINT(p)	((guint) (guint) (p))
@@ -94,27 +95,13 @@ typedef unsigned int guintptr;
 #endif
 
 #define GLIB_MAJOR_VERSION 2
-#define GLIB_MINOR_VERSION 40
-#define GLIB_MICRO_VERSION 0
+#define GLIB_MINOR_VERSION 48
+#define GLIB_MICRO_VERSION 2
 
 #define G_OS_UNIX
 
 
 #define G_VA_COPY	va_copy
-
-#ifdef	__cplusplus
-#define	G_HAVE_INLINE	1
-#else	/* !__cplusplus */
-#define G_HAVE_INLINE 1
-#define G_HAVE___INLINE 1
-#define G_HAVE___INLINE__ 1
-#endif	/* !__cplusplus */
-
-#ifdef	__cplusplus
-#define G_CAN_INLINE	1
-#else	/* !__cplusplus */
-#define G_CAN_INLINE	1
-#endif
 
 #ifndef __cplusplus
 # define G_HAVE_ISO_VARARGS 1
